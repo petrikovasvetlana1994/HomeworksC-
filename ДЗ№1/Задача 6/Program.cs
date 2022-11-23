e8800ff1760c76ad53957d2d7ead273991d65e54
Светлана Петрикова <petrikovasvetlana1994@gmail.com>
2022-11-24 01:40:34 +0300

    задача 6 решена

---
+++ ДЗ№1/Задача 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int d = a % 2;
+
+if (d == 0)
+{
+Console.WriteLine("Четное");
+}
+
+else
+{Console.WriteLine("Нечетное");
+}
