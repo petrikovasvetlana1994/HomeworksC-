64c5f884d75fba3efcefc28d1d1fb7be7f233ecc
Светлана Петрикова <petrikovasvetlana1994@gmail.com>
2022-11-25 21:27:17 +0300

    задача 13 решена

---
+++ ДЗ№2/Задача13/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра числа = " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("третьей цифры нет");
+}
