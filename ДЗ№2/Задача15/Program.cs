559e65101ba551ef84cc8faae803e5173b6291f1
Светлана Петрикова <petrikovasvetlana1994@gmail.com>
2022-11-25 21:51:07 +0300

    задача 15 решена

---
+++ ДЗ№2/Задача15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите цифру, обозначающую день недели(от 1 до 7 включительно): ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("это выходной день");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("в неделе нет столько дней");
+  }
+  else Console.WriteLine("это не выходной день");
