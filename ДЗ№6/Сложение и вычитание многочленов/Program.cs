c73dc398a114573926ebd4fe3a17a6c51b9d446d
Светлана Петрикова <petrikovasvetlana1994@gmail.com>
2022-11-29 22:53:24 +0300

    многочлены

---
+++ ДЗ№6/Сложение и вычитание многочленов/Program.cs
@@ -0,0 +1,47 @@
+﻿// f(x) = 4*x^3 + 3*x^2 + x + 11
+// g(x) = 10*x^3 + x + 10
+// f(x) + g(x) = 14*x^3 + 3*x^2 + 2*x + 21
+// f(x) - g(x) = -6*x^3 + 3*x^2 + 1
+//.........0..1..2..3..4..5.................
+int[] x = {1, 2, 0, 4, 5, 16};
+int[] y = {3, 5};
+// f(x) = 1*x^0 + 2*x^1 + 0*x^2 + 4*x^3 + 5*x^4 + 16*x^5
+// g(x) = 3*x^0 + 5*x^1 + 0*x^2 + 4*x^3 + 5*x^4 + 16*x^5
+
+int[] Sum(int[] f, int[] g)
+{
+    int powF = f.Length;
+    int powG = g.Length;
+
+    int resultMax = powF;
+    int resultMin = powG;
+
+    if (powG > resultMax) 
+    {
+    resultMax = powG;
+    resultMin = powF;
+    }
+
+    int[] result = new int[resultMax];
+
+    for (int i = 0; i < resultMax; i++)
+    {
+        if (i < resultMin)
+       {
+        result[i] = f[i] + g[i];
+       }
+    }
+
+    for (int i = resultMin; i < resultMax; i++)
+    {
+        if (resultMax == powG) result[i] = g[i];
+        else result[i] = f[i];
+    }
+
+    return result;
+
+}
+int[] s = Sum(x, y);
+for (int i = 0; i < s.Length; i++) Console.Write($"{s[i]} ");
+
+System.Console.WriteLine();
