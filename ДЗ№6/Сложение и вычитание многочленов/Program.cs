c7a6949138964b6e24785d2c2a7fac9ddc54ddd1
Светлана Петрикова <petrikovasvetlana1994@gmail.com>
2022-12-07 15:00:57 +0300

    задача 56 решена

--- ДЗ№6/Сложение и вычитание многочленов/Program.cs
+++ ДЗ№6/Сложение и вычитание многочленов/Program.cs
@@ -1,46 +1,41 @@
-﻿// f(x) = 4*x^3 + 3*x^2 + x + 11
+﻿//Урок 6. Задача: Написать программу сложения и вычитания двух многочленов
+// пример с семинара
+// f(x) = 4*x^3 + 3*x^2 + x + 11
 // g(x) = 10*x^3 + x + 10
 // f(x) + g(x) = 14*x^3 + 3*x^2 + 2*x + 21
 // f(x) - g(x) = -6*x^3 + 3*x^2 + 1
-//.........0..1..2..3..4..5.................
-int[] f = {1, 2, 0, 4, 5, 16};
-int[] g = {3, 5};
-// f(x) = 1*x^0 + 2*x^1 + 0*x^2 + 4*x^3 + 5*x^4 + 16*x^5
-// g(x) = 3*x^0 + 5*x^1 + 0*x^2 + 4*x^3 + 5*x^4 + 16*x^5
+
+
 
 // Сложение многочленов
 int[] Sum(int[] f, int[] g)
 {
-    int powF = f.Length;
-    int powG = g.Length;
+  int powF = f.Length;
+  int powG = g.Length;
 
-    int resultMax = powF;
-    int resultMin = powG;
+  int resultMax = powF;
+  int resultMin = powG;
 
-    if (powG > resultMax) 
-    {
+  if (powG > resultMax)
+  {
     resultMax = powG;
     resultMin = powF;
-    }
+  }
 
-    int[] result = new int[resultMax];
+  int[] result = new int[resultMax];
 
-    for (int i = 0; i < resultMax; i++)
-    {
-        
-       {
-        result[i] = f[i] + g[i];
-       }
-    }
+  for (int i = 0; i < resultMin; i++)
+  {
+    result[i] = f[i] + g[i];
+  }
 
-    for (int i = resultMin; i < resultMax; i++)
-    {
-        if (resultMax == powG) result[i] = g[i];
-        else result[i] = f[i];
-    }
+  for (int i = resultMin; i < resultMax; i++)
+  {
+    if (resultMax == powG) result[i] = g[i];
+    else result[i] = f[i];
+  }
 
-    return result;
-
+  return result;
 }
 
 
@@ -105,6 +100,12 @@ string Print(int[] f)
 
   return output;
 }
+
+//.........0..1..2..3..4..5.................
+int[] f = {1, 2, 0, 4, 5, 16};
+int[] g = {3, 5};
+// f(x) = 1*x^0 + 2*x^1 + 0*x^2 + 4*x^3 + 5*x^4 + 16*x^5
+// g(x) = 3*x^0 + 5*x^1 + 0*x^2 + 4*x^3 + 5*x^4 + 16*x^5
 
 Console.Write("Первый многочлен ");
 Console.WriteLine(Print(f));
--- ДЗ№7/Program.cs
+++ ДЗ№7/Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Доделать ввод многочлена
+// https://github.com/iksergey/multi/blob/lesson7/Program.cs
+// *Сделать вывод только нечетных коэффициентов у треугольника распечатайте хотя бы 120 строк
+
+
+int[,] CreateTriangle(int row)
+{
+  int[,] triangle = new int[row, row];
+  for (int i = 0; i < row; i++)
+  {
+    triangle[i, 0] = 1;
+    triangle[i, i] = 1;
+  }
+
+  for (int i = 2; i < row; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      triangle[i, j] =
+      triangle[i - 1, j - 1] + triangle[i - 1, j];
+    }
+  }
+  return triangle;
+}
+
+void PrintTriangle(int[,] triangle)
+{
+    for (int i = 0; i < triangle.GetLength(0); i++)
+  {
+    for (int j = 0; j < triangle.GetLength(1); j++)
+    {
+      if (triangle[i, j] != 0)
+     
+        Console.Write($"{triangle[i, j],6}");
+        
+    }
+    
+    Console.WriteLine();
+  }
+}
+
+
+
+
+int[] GetKoeff(int[,] tr, int pow)
+{
+  int[] row = new int[pow + 1];
+  for (int i = 0; i <= pow; i++)
+  
+  {
+    row[i] = tr[pow, i];
+   }
+  
+  return row;
+}
+
+
+string PrintPolynom(int[] array)
+{
+  string[] pows = { "", "", "^2", "^3", "^4", "^5", "^6", "^7", "^8", "^9" };
+  string output = String.Empty;
+  for (int i = 0; i < array.Length; i++)
+  {
+    int t = array[i];
+    if (array[i] == 0) continue;
+    else if (i != 0) { output += " + "; }
+    
+    
+    if (i == 0) { output += $"a{pows[array.Length-1]}"; }
+    
+    if (i == array.Length - 1) { output += $"b{pows[array.Length-1]}"; }
+    else if (i != 0 && i != array.Length - 1) 
+    { 
+      output += $"{t}a{pows[array.Length - i - 1]}*b{pows[i]}"; 
+    }
+
+  }
+  return output;
+}
+
+
+int[,] tr = CreateTriangle(10);                 // (число) - количество строк
+int row = 2;                                    // степень (a + b)
+PrintTriangle(tr);
+int[] koeff = GetKoeff(tr, row);                
+Console.WriteLine($"(a + b)^{row} = {PrintPolynom(koeff)}");
+Console.WriteLine();
