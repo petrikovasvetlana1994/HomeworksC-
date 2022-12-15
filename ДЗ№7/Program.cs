5f873735986e1bd8499d34c430105e535a5ee552
Светлана Петрикова <petrikovasvetlana1994@gmail.com>
2022-12-15 17:59:51 +0300

    задача 7

--- ДЗ№7/Program.cs
+++ ДЗ№7/Program.cs
@@ -3,19 +3,15 @@
 // *Сделать вывод только нечетных коэффициентов у треугольника распечатайте хотя бы 120 строк
 
 
-int[,] CreateTriangle(int row)
-{
+int[,] CreateTriangle(int row){
   int[,] triangle = new int[row, row];
-  for (int i = 0; i < row; i++)
-  {
+  for (int i = 0; i < row; i++){
     triangle[i, 0] = 1;
     triangle[i, i] = 1;
   }
 
-  for (int i = 2; i < row; i++)
-  {
-    for (int j = 1; j <= i; j++)
-    {
+  for (int i = 2; i < row; i++){
+    for (int j = 1; j <= i; j++){
       triangle[i, j] =
       triangle[i - 1, j - 1] + triangle[i - 1, j];
     }
@@ -23,65 +19,48 @@ int[,] CreateTriangle(int row)
   return triangle;
 }
 
-void PrintTriangle(int[,] triangle)
-{
-    for (int i = 0; i < triangle.GetLength(0); i++)
-  {
-    for (int j = 0; j < triangle.GetLength(1); j++)
-    {
+void PrintTriangle(int[,] triangle){
+  int row = triangle.GetLength(0);
+  for (int i = 0; i < row; i++){
+    for (int j = 0; j < row; j++){
       if (triangle[i, j] != 0)
-     
-        Console.Write($"{triangle[i, j],6}");
-        
+        Console.Write($"{triangle[i, j],4}");
     }
-    
     Console.WriteLine();
   }
 }
 
-
-
-
-int[] GetKoeff(int[,] tr, int pow)
-{
+int[] GetKoeff(int[,] tr, int pow){
   int[] row = new int[pow + 1];
-  for (int i = 0; i <= pow; i++)
-  
-  {
+  for (int i = 0; i <= pow; i++){
     row[i] = tr[pow, i];
-   }
-  
+  }
   return row;
 }
 
-
-string PrintPolynom(int[] array)
+string PrintKoeff(int[] array)
 {
-  string[] pows = { "", "", "^2", "^3", "^4", "^5", "^6", "^7", "^8", "^9" };
   string output = String.Empty;
-  for (int i = 0; i < array.Length; i++)
-  {
-    int t = array[i];
-    if (array[i] == 0) continue;
-    else if (i != 0) { output += " + "; }
-    
-    
-    if (i == 0) { output += $"a{pows[array.Length-1]}"; }
-    
-    if (i == array.Length - 1) { output += $"b{pows[array.Length-1]}"; }
-    else if (i != 0 && i != array.Length - 1) 
-    { 
-      output += $"{t}a{pows[array.Length - i - 1]}*b{pows[i]}"; 
-    }
-
+  int length = array.Length;
+  for (int i = 0; i < length; i++){
+    if (i != 0) { output += " + "; }
+    if((length - 1 - i) == 0){output += $"b^{i}"; }
+    if(i == 0){output += $"a^{length - 1 - i}"; }
+    if(length == 3 & array[i] != 1 & (length - 1 - i) != 0 & i != 0 & (length - 1 - i) == 1 & i == 1) {output += $"{array[i]}ab"; }
+    if(length != 3 & array[i] == 1 & (length - 1 - i) != 0 & i != 0 & i != 1) {output += $"a^{length - 1 - i}b^{i}"; }
+    if(length != 3 & array[i] != 1 & (length - 1 - i) != 0 & i != 0 & (length - 1 - i) == 1) {output += $"{array[i]}ab^{i}"; }
+    if(length != 3 & array[i] != 1 & (length - 1 - i) != 0 & i != 0 & i == 1) {output += $"{array[i]}a^{length - 1 - i}b"; }    
+    if(length != 3 & array[i] != 1 & (length - 1 - i) != 0 & i != 0 & (length - 1 - i) == 1 & i == 1) {output += $"{array[i]}ab"; }    
+    if(length != 3 & array[i] != 1 & (length - 1 - i) != 0 & i != 0 & (length - 1 - i) != 1 & i != 1) {output += $"{array[i]}a^{length - 1 - i}b^{i}"; }
   }
-  return output;
+    return output;
 }
 
-
-int[,] tr = CreateTriangle(10);                 // (число) - количество строк
-int row = 2;                                    // степень (a + b)
+int[,] tr = CreateTriangle(120);
 PrintTriangle(tr);
-int[] koeff = GetKoeff(tr, row);                
-Console.WriteLine($"(a + b)^{row} = {PrintPolynom(koeff)}");
+int row = 2; 
+int[] array = GetKoeff(tr, row);
+Console.WriteLine();
+Console.WriteLine(String.Join(' ', array));
+Console.WriteLine($"(a+b)^{row} = {PrintKoeff(array)}");
 Console.WriteLine();
